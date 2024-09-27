39851471360b107e9850b6813487b30e89e8dcc4
Branchertefa <105944100+Branchertefa@users.noreply.github.com>
2024-09-27 08:24:38 -0300

    Create informaçoesGlobais.js

---
+++ graficos/informaçoesGlobais.js
@@ -0,0 +1,12 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function visualizarInformaçoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem`
+}
+
+visualizarInformaçoesGlobais()
